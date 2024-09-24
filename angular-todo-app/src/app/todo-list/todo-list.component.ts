074cbb067d857f8e4e2edbc7ec784ad52ef8479c
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-8.local>
2024-09-24 17:57:37 +0100

    adds api methods to link to python app

--- angular-todo-app/src/app/todo-list/todo-list.component.ts
+++ angular-todo-app/src/app/todo-list/todo-list.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../todo.service';
 
 @Component({
   selector: 'app-todo-list',
-  standalone: true,
-  imports: [],
   templateUrl: './todo-list.component.html',
-  styleUrl: './todo-list.component.css'
+  styleUrls: ['./todo-list.component.css'],
+  standalone: true
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+  todos: any[] = [];
+  newTodo: string = '';
 
+  constructor(private todoService: TodoService) {}
+
+  ngOnInit(): void {
+    this.fetchTodos();
+  }
+
+  fetchTodos(): void {
+    this.todoService.getTodos().subscribe(todos => {
+      this.todos = todos;
+    });
+  }
+
+  addTodo(): void {
+    if (this.newTodo.trim()) {
+      const todo = { title: this.newTodo };
+      this.todoService.addTodo(todo).subscribe(todo => {
+        this.todos.push(todo);
+        this.newTodo = '';  
+      });
+    }
+  }
+
+  deleteTodo(index: number): void {
+    this.todoService.deleteTodo(index).subscribe(() => {
+      this.todos.splice(index, 1);  
+    });
+  }
 }
+
--- angular-todo-app/src/app/todo.service.ts
+++ angular-todo-app/src/app/todo.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  private apiUrl = 'http://localhost:5000/api/todos';  
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+ 
+  getTodos(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+
+  
+  addTodo(todo: any): Observable<any> {
+    return this.http.post(this.apiUrl, todo);
+  }
+
+  deleteTodo(index: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${index}`);
+  }
 }
+
